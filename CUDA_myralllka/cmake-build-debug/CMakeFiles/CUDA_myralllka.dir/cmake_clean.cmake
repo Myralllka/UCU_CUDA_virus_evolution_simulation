081f8e27644687619b15012abd4f8ed40215b113
Mykola Morgunenko <Olli25010@gmail.com>
2020-04-10 11:23:56 +0300

    add Myalllka's CUDA

---
+++ CUDA_myralllka/cmake-build-debug/CMakeFiles/CUDA_myralllka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CUDA_myralllka.dir/first.cu.o"
+  "CUDA_myralllka"
+  "CUDA_myralllka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/CUDA_myralllka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
